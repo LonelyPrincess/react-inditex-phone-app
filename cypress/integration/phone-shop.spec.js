a79bf28b73ff02c287121c14f1e71f02bfca8182
Sara Hernández Suárez <sara.her.su@gmail.com>
2022-01-02 18:29:38 +0000

    test: add blank test suite for phone list page

---
+++ cypress/integration/phone-shop.spec.js
@@ -0,0 +1,9 @@
+context('Phone list page', () => {
+  beforeEach(() => {
+    cy.visit('https://localhost:3000');
+  });
+
+  it('should display list of available phones', () => {
+    // TODO
+  });
+});
