f98170c16fce61695c9f4c10627c5cf77102b0e5
Sara Hernández Suárez <sara.her.su@gmail.com>
2022-01-03 14:23:43 +0000

    feat: define routes for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import ProductList from './pages/ProductList/ProductList';
+import ProductDetails from './pages/ProductDetails/ProductDetails';
 
 const App = () => {
   console.log('todo');
   return (
     <div className="App">
       <Header />
-      <ProductList />
+      <Routes>
+        <Route
+          path="/products"
+          element={<ProductList />}
+        />
+        <Route
+          path="/products/:id"
+          element={<ProductDetails />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to="/products" />}
+        />
+      </Routes>
     </div>
   );
 };
