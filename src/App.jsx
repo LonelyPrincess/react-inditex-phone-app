60a3c7b2494dca5b2f0f6cb9a72ba9ba14f3b464
Sara Hernández Suárez <sara.her.su@gmail.com>
2022-01-04 22:20:46 +0000

    refactor: remove console.log in app component

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,26 @@ import Header from './components/Header/Header';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
 
-const App = () => {
-  console.log('todo');
-  return (
-    <Container>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/products"
-            element={<ProductList />}
-          />
-          <Route
-            path="/products/:productId"
-            element={<ProductDetails />}
-          />
-          <Route
-            path="*"
-            element={<Navigate to="/products" />}
-          />
-        </Routes>
-      </main>
-    </Container>
-  );
-};
+const App = () => (
+  <Container>
+    <Header />
+    <main>
+      <Routes>
+        <Route
+          path="/products"
+          element={<ProductList />}
+        />
+        <Route
+          path="/products/:productId"
+          element={<ProductDetails />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to="/products" />}
+        />
+      </Routes>
+    </main>
+  </Container>
+);
 
 export default App;
