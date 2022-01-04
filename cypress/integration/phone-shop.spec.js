107d6d8cbfc215bedf637ed209bdb20a4ee72b0d
Sara Hernández Suárez <sara.her.su@gmail.com>
2022-01-04 11:16:47 +0000

    test: check all products are displayed on screen

--- cypress/integration/phone-shop.spec.js
+++ cypress/integration/phone-shop.spec.js
@@ -1,9 +1,18 @@
 context('Phone list page', () => {
   beforeEach(() => {
-    cy.visit('https://localhost:3000');
+    cy.visit('/');
+
+    cy.intercept({
+      method: 'GET',
+      url: `${Cypress.env('API_BASE_PATH')}/products`,
+    }, {
+      delay: 2000,
+      fixture: 'get-product-list.json',
+    }).as('getProductListRequest');
   });
 
   it('should display list of available phones', () => {
-    // TODO
+    cy.wait('@getProductListRequest');
+    cy.get('[data-cy=product-list-item]').should('have.length', 100);
   });
 });
