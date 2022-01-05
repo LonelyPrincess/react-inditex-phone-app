8c4fdac5b3983dec9e25f365fe9db3f7b91b2453
Sara Hernández Suárez <sara.her.su@gmail.com>
2022-01-05 14:17:46 +0000

    refactor: remove sample counter reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
 import productReducer from '../state/productSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     product: productReducer,
   },
 });
