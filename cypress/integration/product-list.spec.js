8f2aa626b5b4d5400ec1ac41d95f55d0f10b9ee9
Sara Hernández Suárez <sara.her.su@gmail.com>
2022-01-05 17:04:02 +0000

    test: improve tests for product list page

---
+++ cypress/integration/product-list.spec.js
@@ -0,0 +1,49 @@
+context('Product list page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+
+    cy.intercept({
+      method: 'GET',
+      url: `${Cypress.env('API_BASE_PATH')}/products`,
+    }, {
+      fixture: 'get-product-list.json',
+    }).as('getProductListRequest');
+  });
+
+  it('should display list of available products', () => {
+    cy.wait('@getProductListRequest');
+    cy.get('[data-cy=product-list-item]').should('have.length', 100);
+
+    cy.get('[data-cy=search-term]').should('not.exist');
+    cy.get('[data-cy=search-results-count]').should('not.exist');
+    cy.get('[data-cy=all-products-count]').should('have.text', 100);
+
+    cy.get('[data-cy=product-search-box]').type('Alcatel');
+    cy.get('[data-cy=product-list-item]').should('have.length', 1);
+
+    cy.get('[data-cy=search-term]').should('have.text', 'Alcatel');
+    cy.get('[data-cy=search-results-count]').should('have.text', 1);
+    cy.get('[data-cy=all-products-count]').should('have.text', 100);
+
+    cy.get('[data-cy=product-search-box]').clear();
+    cy.get('[data-cy=product-list-item]').should('have.length', 100);
+  });
+
+  it('should be able to navigate to product details', () => {
+    cy.wait('@getProductListRequest');
+
+    cy.intercept({
+      method: 'GET',
+      url: `${Cypress.env('API_BASE_PATH')}/products/q7dTIKOZuH9JA6CI_Ra6e`,
+    }, {
+      fixture: 'get-product-details.json',
+    }).as('getProductDetails');
+
+    cy.get('[data-cy=product-search-box]').type('Allegro');
+    cy.get('[data-cy=product-list-item]')
+      .first()
+      .click();
+
+    cy.url().should('match', /\/products\/q7dTIKOZuH9JA6CI_Ra6e/);
+  });
+});
